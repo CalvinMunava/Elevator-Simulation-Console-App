d3fe04b9bd6bdc278c83fee9a83c122054a4e8b2
Calvin Munava <calvinmunava@gmail.com>
2024-03-26 09:07:36 +0200

    Added Initial Program Entry Point {Main}

--- elevator_app/Program.cs
+++ elevator_app/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace elevator_app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
