18f09544cd8efc12747963faff4349a676bb2324
Calvin Munava <calvinmunava@gmail.com>
2024-03-27 11:37:49 +0200

    Added Initial Console launch Joke and Joke Class
    Added Folders for Classes and Enums

---
+++ elevator_app/Classes/ElevatorJoke.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevator_app.Classes
+{
+    public class ElevatorJoke
+    {
+        public int jokeID { get; set; }
+        public string jokeText { get; set; }
+        public static ElevatorJoke GetJoke()
+        {
+            Random random = new Random();
+            int selectedJoke = random.Next(0, 6);
+            var Jokes = new ElevatorJoke[]
+            {
+                new ElevatorJoke { jokeID = 1 , jokeText = "I've recently developed a severe phobia of elevators....\nI'm taking steps to avoid them."},
+                new ElevatorJoke { jokeID = 2 , jokeText = "I like elevator jokes....\nThey're very uplifting"},
+                new ElevatorJoke { jokeID = 3 , jokeText = "My twin brother prefers to take the stairs, but I like the elevator....\nI guess we are raised differently."},
+                new ElevatorJoke { jokeID = 4 , jokeText = "What did the American elevator say to the British elevator?...\nYou lift bro?"},
+                new ElevatorJoke { jokeID = 5 , jokeText = "My first time using an elevator was an uplifting experience....\nThe second time let me down"},
+                new ElevatorJoke { jokeID = 6 , jokeText = "You should get a job as an elevator....\nIt’s easy to get a raise!"},
+            };
+            return Jokes[selectedJoke];
+        }
+    }
+}
--- elevator_app/Program.cs
+++ elevator_app/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using elevator_app.Classes;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
@@ -10,7 +11,32 @@ namespace elevator_app
     {
         static async Task Main(string[] args)
         {
+            // Set Console Settings
+            Console.Title = "Elevator Simulation Program";
+
+            // Call Console Greetings
+            Greeting();
 
         }
+
+        // Console App Greeting Method
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to Elevator Admin Console");
+            Console.WriteLine("Here's your complimentary Joke of the Day :)");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(ElevatorJoke.GetJoke().jokeText); // Display App Greeting Joke lol
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+
+
+
+
+
+
+
     }
 }
